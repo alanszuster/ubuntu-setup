9bf4f6980af65fc1303e869691367e710293580c
Alan Szuster <alan.szuster@sallinggroup.com>
2026-01-22 20:15:10 +0100

    Fix spacing in PS1 variable for better readability in terminal prompt

--- show-gitbranch-terminal/show-gitbranch-terminal.sh
+++ show-gitbranch-terminal/show-gitbranch-terminal.sh
@@ -5,8 +5,8 @@ parse_git_branch() {
 }
 
 if [ "$color_prompt" = yes ]; then
- PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\$ '
+ PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\] $(parse_git_branch)\[\033[00m\]\$ '
 else
- PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\w$(parse_git_branch)\$ '
+ PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\w $(parse_git_branch)\$ '
 fi
 unset color_prompt force_color_prompt
